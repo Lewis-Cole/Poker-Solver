7787248acd22b893e0b11d077cef81603a42e3a0
Lewis-Cole <Lewis-Cole@users.noreply.github.com>
2019-07-24 18:37:56 +0100

    Take two holdings and some portion of the board and return montecarlo equity sim

--- equitycalc.py
+++ equitycalc.py
@@ -1,39 +1,34 @@
-''' Calculate equity of two hands in given scenario.
-'''
-
-from deck import full_deck, get_card
-from hand_values import compare_hands
-
+from card import Card, card_ranks, card_suits
+from hand import Hand
+from deck import Deck
+from comparehands import compare_hands
 
 def calculate_equity(IP_holding, OOP_holding, board):
+    deck = Deck(card_ranks, card_suits)
     dead_cards = IP_holding + OOP_holding + board
-    deck = []
-    for i in full_deck:
-        deck.append(i)
-    for i in dead_cards:
-        deck.remove(i)
+    deck.remove_dead_cards(dead_cards)
     cards_to_deal = 5 - len(board)
-    
+
     IP_wins = 0.0
     OOP_wins = 0.0
 
     while True:
-        for i in range (100000):
-            onto_board = list(get_card(deck, cards_to_deal))
-            total_board = board + onto_board
-            IP_cards = IP_holding + total_board
-            OOP_cards = OOP_holding + total_board
-            x = compare_hands(IP_cards, OOP_cards)
-            if x == 'IP wins':
+        for _ in range(10000):
+            onto_board = list(deck.get_cards(cards_to_deal))
+            IP_hand = Hand(IP_holding + board + onto_board)
+            OOP_hand = Hand(OOP_holding + board + onto_board)
+            result = compare_hands(IP_hand, OOP_hand)
+            if result == 'IP wins':
                 IP_wins += 1
-            elif x == 'OOP wins':
+            elif result == 'OOP wins':
                 OOP_wins += 1
-            elif x == 'Split pot':
+            else:
                 IP_wins += 0.5
                 OOP_wins += 0.5
-        IP_EQ = 100 * IP_wins / (IP_wins + OOP_wins)
-        OOP_EQ = 100 * OOP_wins / (IP_wins + OOP_wins)
+            IP_EQ = 100 * IP_wins / (IP_wins + OOP_wins)
+            OOP_EQ = 100 * OOP_wins / (IP_wins + OOP_wins)
         print('IP equity = ' + str(IP_EQ) + '%')
         print('OOP equity = ' + str(OOP_EQ) + '%')
     return (IP_EQ, OOP_EQ)
 
+calculate_equity([Card('2', 's'), Card('K', 's')],[Card('8', 'd'), Card('7', 'h')],[])
