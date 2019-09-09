5fe3358fa3f64a6f9c12ae5f7f1d809155da4b0a
Lewis-Cole <Lewis-Cole@users.noreply.github.com>
2019-09-09 20:52:22 +0100

    same as previous

--- hrange.py
+++ hrange.py
@@ -1,13 +1,13 @@
 from hand import Hand
 
-class Range:
+class HRange:
 
     def __init__(self, hands):
         self.hands = hands
-        if not(self.validate_range()):
+        if not(self.validate_hrange()):
             raise ValueError('Invalid range input')
     
-    def validate_range(self):
+    def validate_hrange(self):
         if type(self.hands) != list:
             return False
         for hand in self.hands:
